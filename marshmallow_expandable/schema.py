28edaa1bfc91e3d96f1c32d15dd2d2d83a73f038
David Jiménez <davigetto@gmail.com>
2018-10-12 16:58:06 +0200

    Add a batch option to increase performance

--- marshmallow_expandable/schema.py
+++ marshmallow_expandable/schema.py
@@ -1,4 +1,9 @@
+import logging
+
 from marshmallow import fields
+
+
+logger = logging.getLogger(__name__)
 
 
 class ExpandableSchemaMixin(object):
@@ -49,47 +54,89 @@ class ExpandableNested(fields.Nested):
         return schema
 
     def _get_interactor_and_params(self):
-        if not hasattr(self.schema.Meta, 'query_function'):
+        if not hasattr(self.schema.Meta, 'retrieve'):
             raise Exception('The nested class does not have an "query_function" Meta '
                             'attribute, therefore it does not support expansion')
 
         try:
-            query_function_method, parameters = self.schema.Meta.query_function
+            retrieve_func, retrieve_arguments = self.schema.Meta.retrieve
         except Exception as e:
             raise Exception('The interactor Meta attribute should be a tuple composed '
                             'by the function to build the interactor and the list of parameters') from e
 
-        query_param_map = {}
+        try:
+            batch_func, batch_arguments = self.schema.Meta.batch
+        except Exception:
+            batch_func, batch_arguments = None, None
+
+        retrieve_argument_map = {}
         """Maps a queryparam from the schema to the interactor"""
 
-        for param in parameters:
-            if isinstance(param, tuple):
-                param_in_interactor, param_in_schema = param
-                query_param_map[param_in_interactor] = param_in_schema
+        for arg in retrieve_arguments:
+            if isinstance(arg, tuple):
+                arg_in_interactor, arg_in_schema = arg
+                retrieve_argument_map[arg_in_interactor] = arg_in_schema
             else:
-                query_param_map[param] = param
+                retrieve_argument_map[arg] = arg
 
-        return query_function_method, query_param_map
+        batch_argument_map = {}
+        if batch_func:
+            for arg in batch_arguments:
+                arg_in_interactor, arg_in_schema = arg
+                batch_argument_map[arg_in_interactor] = arg_in_schema
+
+        return retrieve_func, retrieve_argument_map, batch_func, batch_argument_map
 
     def _serialize(self, nested_obj, attr, obj):
         resource = nested_obj
+
         if self._should_expand(attr):
-            query_function_method, query_param_map = self._get_interactor_and_params()
-            qparams = self._get_search_parameters(nested_obj, query_param_map)
-            resource_or_interactor = query_function_method(**qparams)
+            retrieve_func, argument_map, batch_func, batch_argument_map = self._get_interactor_and_params()
 
-            if self._is_interactor(resource_or_interactor):
-                resource_or_interactor = resource_or_interactor.execute()
+            if self.many and batch_func:
+                arguments = self._generate_batch_arguments(nested_obj, batch_argument_map)
+                result = self._execute_query(batch_func, arguments)
+            else:
+                arguments = self._generate_arguments(nested_obj, argument_map)
 
-            resource = resource_or_interactor
+                if self.many:
+                    result = [self._execute_query(retrieve_func, qparam) for qparam in arguments]
+                else:
+                    result = self._execute_query(retrieve_func, arguments)
+
+            resource = result
 
         return super()._serialize(resource, attr, obj)
+
+    def _execute_query(self, retrieve_func, qparams):
+        resource_or_interactor = retrieve_func(**qparams)
+
+        if self._is_interactor(resource_or_interactor):
+            result = resource_or_interactor.execute()
+        else:
+            result = resource_or_interactor
+
+        return result
 
     def _is_interactor(self, resource_or_interactor):
         return hasattr(resource_or_interactor, 'execute')
 
-    def _get_search_parameters(self, obj, params):
-        return {query_param: obj[attr] for query_param, attr in params.items()}
+    def _generate_batch_arguments(self, obj, argument_map):
+        assert self._is_iterable(obj), 'The object is not a list as expected!'
+        return {argument: [o[attribute_in_schema] for o in obj] for argument, attribute_in_schema in argument_map.items()}
+
+    def _generate_arguments(self, obj, argument_map):
+        if self.many:
+            return [{argument: o[attribute_in_schema] for argument, attribute_in_schema in argument_map.items()} for o in obj]
+        else:
+            return {argument: obj[attribute_in_schema] for argument, attribute_in_schema in argument_map.items()}
+
+    def _is_iterable(self, obj):
+        try:
+            iter(obj)
+            return True
+        except TypeError:
+            return False
 
     def _should_expand(self, attr):
         # The only one that knows if we should expand this attribute or not is the root schema
---
+++ tests/test_schema.py
@@ -0,0 +1,157 @@
+from marshmallow import Schema, fields
+
+from marshmallow_expandable import ExpandableSchemaMixin
+from marshmallow_expandable.schema import ExpandableNested
+
+
+def get_my_nested_schema(id):
+    return dict(id=id, attr3=f'pine-{id}', attr4=f'pineapple-{id}', attr7={'id': 7})
+
+
+def get_my_nested_schema_batch(ids):
+    return [get_my_nested_schema(i) for i in ids]
+
+
+def get_another_nested_schema(my_id):
+    return dict(id=my_id, attr5=f'apple-{my_id}',
+                attr6=[{'id': get_my_nested_schema(i)['id']} for i in range(1, 3)])
+
+
+class AnotherNestedSchema(ExpandableSchemaMixin, Schema):
+    id = fields.Int()
+    attr5 = fields.Str()
+    attr6 = ExpandableNested('MyNestedSchema', many=True)
+
+    class Meta:
+        retrieve = get_another_nested_schema, [('my_id', 'id')]
+
+
+class MySchema(ExpandableSchemaMixin, Schema):
+    id = fields.Int()
+    attr1 = fields.Str()
+    attr2 = fields.Str()
+    attr3 = ExpandableNested('MyNestedSchema')
+    attr4 = ExpandableNested(AnotherNestedSchema, many=True)
+
+
+class MyNestedSchema(ExpandableSchemaMixin, Schema):
+    id = fields.Int()
+    attr3 = fields.Str()
+    attr4 = fields.Str()
+    attr7 = ExpandableNested('self')
+
+    class Meta:
+        retrieve = get_my_nested_schema, ['id']
+        batch = get_my_nested_schema_batch, [('ids', 'id')]
+
+
+sample_json = dict(
+    id=1,
+    attr1='banana',
+    attr2='potato',
+    attr3=dict(
+        id=4
+    ),
+    attr4=[
+        dict(id=1), dict(id=2), dict(id=3)
+    ]
+)
+
+class TestExpandableSchemaMixin:
+    def test_serialize(self):
+        result, errors = MySchema(expand=['attr3']).dump(sample_json)
+
+        expected_result = dict(
+            id=1,
+            attr1='banana',
+            attr2='potato',
+            attr3=dict(
+                id=4,
+                attr3='pine-4',
+                attr4='pineapple-4'
+            ),
+            attr4=[
+                dict(id=1), dict(id=2), dict(id=3)
+            ]
+        )
+
+        assert expected_result == result
+
+    def test_serialize_many(self):
+        result, errors = MySchema(expand=['attr4']).dump(sample_json)
+
+        expected_result = dict(
+            id=1,
+            attr1='banana',
+            attr2='potato',
+            attr3=dict(
+                id=4
+            ),
+            attr4=[
+                dict(id=1, attr5='apple-1', attr6=[dict(id=1), dict(id=2)]),
+                dict(id=2, attr5='apple-2', attr6=[dict(id=1), dict(id=2)]),
+                dict(id=3, attr5='apple-3', attr6=[dict(id=1), dict(id=2)])
+            ]
+        )
+
+        assert expected_result == result
+
+    def test_serialize_many_nested(self):
+        result, errors = MySchema(expand=['attr4.attr6', 'attr3']).dump(sample_json)
+
+        expected_result = dict(
+            id=1,
+            attr1='banana',
+            attr2='potato',
+            attr3=dict(
+                id=4,
+                attr3='pine-4',
+                attr4='pineapple-4',
+                attr7={'id': 7}
+            ),
+            attr4=[
+                dict(id=1, attr5='apple-1', attr6=[dict(id=1, attr3='pine-1', attr7={'id': 7}, attr4='pineapple-1'),
+                                                   dict(id=2, attr3='pine-2', attr7={'id': 7}, attr4='pineapple-2')]),
+                dict(id=2, attr5='apple-2', attr6=[dict(id=1, attr3='pine-1', attr7={'id': 7}, attr4='pineapple-1'),
+                                                   dict(id=2, attr3='pine-2', attr7={'id': 7}, attr4='pineapple-2')]),
+                dict(id=3, attr5='apple-3', attr6=[dict(id=1, attr3='pine-1', attr7={'id': 7}, attr4='pineapple-1'),
+                                                   dict(id=2, attr3='pine-2', attr7={'id': 7}, attr4='pineapple-2')])
+            ]
+        )
+
+        assert expected_result == result
+
+    def test_three_levels_of_nesting(self):
+        sample_json = dict(
+            id=1,
+            attr1='banana',
+            attr2='potato',
+            attr3=dict(
+                id=4
+            ),
+            attr4=[
+                dict(id=1)
+            ]
+        )
+
+        samey = {'id': 7, 'attr3': 'pine-7', 'attr4': 'pineapple-7', 'attr7': {'id': 7}}
+
+        result, errors = MySchema(expand=['attr4.attr6.attr7']).dump(sample_json)
+
+        expected_result = dict(
+            id=1,
+            attr1='banana',
+            attr2='potato',
+            attr3=dict(
+                id=4
+            ),
+            attr4=[
+                dict(id=1, attr5='apple-1', attr6=[dict(id=1, attr3='pine-1', attr7=samey, attr4='pineapple-1'),
+                                                   dict(id=2, attr3='pine-2', attr7=samey, attr4='pineapple-2')]),
+            ]
+        )
+
+        assert expected_result == result
+
+    def test_if_batch_function_is_present_then_use_the_batch_version(self):
+        pass
